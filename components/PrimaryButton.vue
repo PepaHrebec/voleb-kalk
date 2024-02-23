2c9ebcd5201c19c706c2d8b1821951e40fe519b8
Josef Hrebec <josefhrebec02@gmail.com>
2024-02-23 15:14:40 +0100

    Add index page basics

---
+++ components/PrimaryButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  content: string;
+}>();
+</script>
+
+<template>
+  <button>{{ content }}</button>
+</template>
+
+<style>
+button {
+  padding: 8px 24px;
+  background-color: var(--primary-blue);
+  color: white;
+  border-radius: 8px;
+  font-weight: bold;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,32 @@
 <script lang="ts"></script>
 
-<template></template>
+<template>
+  <div class="names-flex-wrapper">
+    <div class="names-wrapper">
+      <h3>Švehla?</h3>
+      <h3>Kramář?</h3>
+      <h3>Komunismus?</h3>
+      <h3 class="names-grayed">A co Henlein?</h3>
+    </div>
+  </div>
+  <NuxtLink to="/about"
+    ><PrimaryButton content="Spustit kalkulačku ->"
+  /></NuxtLink>
+</template>
+
+<style scoped>
+.names-flex-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 64px 0;
+}
+
+.names-grayed {
+  color: var(--text-gray);
+}
+
+h3 {
+  font-size: 1.6em;
+}
+</style>
