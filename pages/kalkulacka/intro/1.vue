0b193e474f82822dda057bac060d7af9e2f069a4
Josef Hrebec <josefhrebec02@gmail.com>
2024-02-27 20:03:14 +0100

    Add intro pages

---
+++ pages/kalkulacka/intro/1.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <QuestionCard
+    title="Úvod"
+    :current-question-num="1"
+    :list-length="2"
+    :is-intro="true"
+  />
+  <IntroButtons
+    forward="/kalkulacka/intro/2"
+    back="/"
+    forward-text="Další"
+    back-text="Menu"
+  />
+</template>
---
+++ pages/kalkulacka/intro/2.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <QuestionCard
+    title="Úvod 2"
+    :current-question-num="2"
+    :list-length="2"
+    :is-intro="true"
+  />
+  <IntroButtons
+    forward="/kalkulacka/otazky/1"
+    back="/kalkulacka/intro/1"
+    forward-text="Otázky"
+    back-text="Zpět"
+  />
+</template>
+
+<style scoped></style>
