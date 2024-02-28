6e4dc88a86607d309c511028008c84ea4a5b24b0
Josef Hrebec <josefhrebec02@gmail.com>
2024-02-28 22:46:32 +0100

    Add buttons to submit answer component

---
+++ components/AnswerButtons.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="wrap">
+    <SecondaryButton text="Silně ano" :red="false" />
+    <SecondaryButton text="Ano" :red="false" />
+    <SecondaryButton text="Nevím" class="middle" :red="false" />
+    <SecondaryButton text="Ne" :red="false" />
+    <SecondaryButton text="Silně ne" :red="false" />
+  </div>
+</template>
+
+<style scoped>
+.wrap {
+  width: auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 8px;
+}
+
+.middle {
+  grid-column: 1/3;
+}
+</style>
