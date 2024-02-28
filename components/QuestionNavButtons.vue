2d80edebf223e2ef661029f4ad7e1d98182a0bc9
Josef Hrebec <josefhrebec02@gmail.com>
2024-02-28 21:42:45 +0100

    Add button to navigate questions

---
+++ components/QuestionNavButtons.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+defineProps<{
+  currentQuestionNum: number | undefined;
+  listLength: number | undefined;
+}>();
+</script>
+
+<template>
+  <div
+    v-if="currentQuestionNum !== undefined && listLength !== undefined"
+    class="link-wrap"
+  >
+    <NuxtLink
+      v-if="currentQuestionNum !== 1"
+      :to="`/kalkulacka/otazky/` + (currentQuestionNum - 1)"
+      class="arrow"
+      >&lt;-</NuxtLink
+    >
+    <div class="arrow disabled" v-else>&lt;-</div>
+    <NuxtLink
+      class="arrow"
+      v-if="currentQuestionNum !== listLength"
+      :to="`/kalkulacka/otazky/` + (currentQuestionNum + 1)"
+      >-&gt;</NuxtLink
+    >
+    <div class="arrow disabled" v-else>-&gt;</div>
+  </div>
+</template>
+
+<style scoped>
+.link-wrap {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 20px;
+}
+
+.arrow {
+  text-decoration: none;
+  font-size: 1.8em;
+  color: black;
+}
+
+.disabled {
+  color: rgb(157, 157, 157);
+}
+</style>
