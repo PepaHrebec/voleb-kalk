1c374c87f7324c6256753449c00c3903124ef651
Josef Hrebec <josefhrebec02@gmail.com>
2024-03-04 22:40:24 +0100

    Update front page

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,34 @@
 <script lang="ts"></script>
 
 <template>
-  <div class="names-flex-wrapper">
-    <div class="names-wrapper">
-      <h3>Švehla?</h3>
-      <h3>Kramář?</h3>
-      <h3>Komunismus?</h3>
-      <h3 class="names-grayed">A co Henlein?</h3>
+  <main>
+    <div class="names-flex-wrapper">
+      <div class="names-wrapper">
+        <h3>Švehla?</h3>
+        <h3>Kramář?</h3>
+        <h3>Komunismus?</h3>
+        <h3 class="names-grayed">A co Henlein?</h3>
+      </div>
     </div>
-  </div>
-  <div class="middle">
-    <NuxtLink to="/kalkulacka/intro"
-      ><PrimaryButton content="Spustit kalkulačku ->"
-    /></NuxtLink>
-    <NuxtLink class="link-parties" to="/strany"
-      >Strany, které v kalkulačce najdete -></NuxtLink
-    >
-  </div>
-  <div class="about-wrap">
-    <h2>O projektu</h2>
-    <p>
-      Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-      Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-      Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-      Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-      Text Text Text Text Text Text Text Text Text Text
-    </p>
-  </div>
+    <div class="middle">
+      <NuxtLink to="/kalkulacka/intro/1"
+        ><PrimaryButton content="Spustit kalkulačku ->"
+      /></NuxtLink>
+      <NuxtLink class="link-parties" to="/strany"
+        >Strany, které v kalkulačce najdete -></NuxtLink
+      >
+    </div>
+    <div class="about-wrap">
+      <h2>O projektu</h2>
+      <p>
+        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+      </p>
+    </div>
+  </main>
 </template>
 
 <style scoped>
@@ -34,7 +36,7 @@
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 64px 0;
+  padding-bottom: 64px;
 }
 
 .names-grayed {
