641474e6a32d5625d147657fffe51219f8e02d16
Josef Hrebec <josefhrebec02@gmail.com>
2024-03-04 22:39:14 +0100

    Add summary card

---
+++ components/SummaryCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useAnswersStore } from "~/scripts/answersStore";
+import type { Question } from "~/types/types";
+
+defineProps<{
+  question: Question;
+  questionNumber: number;
+}>();
+
+const answersStore = useAnswersStore();
+const { answersList } = storeToRefs(answersStore);
+</script>
+<template></template>
+<style scoped></style>
