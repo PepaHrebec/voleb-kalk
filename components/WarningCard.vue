d7b20400524514ba74ecc90d02dab93a8254c4d2
Josef Hrebec <josefhrebec02@gmail.com>
2024-03-10 22:23:04 +0100

    Add card displaying a warning

---
+++ components/WarningCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  content: string;
+}>();
+</script>
+<template>
+  <div class="warning-wrap">
+    <h3>{{ title }}</h3>
+    <p>{{ content }}</p>
+  </div>
+</template>
+<style>
+.warning-wrap {
+  border: dashed var(--primary-red);
+  background-color: rgb(254, 214, 221);
+  padding: 12px;
+  color: var(--primary-red);
+  border-radius: 8px;
+}
+</style>
