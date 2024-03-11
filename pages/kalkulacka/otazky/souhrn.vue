0db9a5185f7994c5c7597c6b16b7f4025329882c
Josef Hrebec <josefhrebec02@gmail.com>
2024-03-11 15:41:48 +0100

    Create the summary page

---
+++ pages/kalkulacka/otazky/souhrn.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import useResult from "~/composables/useResult";
+import { useQuestionsStore } from "~/scripts/questionsStore";
+
+const trye = await useResult();
+
+console.log(trye);
+
+const questionsStore = useQuestionsStore();
+const { questionsList } = storeToRefs(questionsStore);
+</script>
+<template>
+  <h2>Souhrn</h2>
+  <div class="summary-card-wrap">
+    <SummaryCard
+      v-for="(question, index) in questionsList"
+      :question-number="index + 1"
+      :question="question"
+      :questions-length="questionsList?.length"
+    />
+  </div>
+  <WarningCard
+    title="Varování"
+    content='Po odeslání budou Vaše nezodpovězené otázky počítány jako "Nevím". Zkontrolujte si, jestli jste nějaké neopomněl.'
+  />
+</template>
+<style scoped>
+.summary-card-wrap {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  padding-top: 12px;
+  padding-bottom: 24px;
+}
+</style>
