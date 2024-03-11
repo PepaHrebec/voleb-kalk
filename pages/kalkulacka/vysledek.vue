c6c9e52da1c5caa1bf3b3336d99e1aff450d6efe
Josef Hrebec <josefhrebec02@gmail.com>
2024-03-11 21:44:47 +0100

    Add results page

---
+++ pages/kalkulacka/vysledek.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import useResult from "~/composables/useResult";
+const results = await useResult();
+</script>
+<template>
+  <h2>Výsledky</h2>
+  <div class="results-wrap">
+    <ResultsCard v-for="result in results" :results="result" />
+  </div>
+</template>
+<style scoped>
+.results-wrap {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  padding: 24px 0;
+}
+</style>
