0be24efeb92bce9127f458eac76b4af1435b03cb
Josef Hrebec <josefhrebec02@gmail.com>
2024-03-14 23:18:56 +0100

    Add Party card component

--- pages/strany.vue
+++ pages/strany.vue
@@ -1,29 +1,25 @@
 <script setup lang="ts">
-import type { ParsedContent } from "@nuxt/content/dist/runtime/types";
-
-interface ParsedParties extends ParsedContent {
-  parties: {
-    name: string;
-    description: string;
-    circleColor: string;
-  }[];
-}
+import type { ParsedParties } from "~/types/types";
 
 const { data } = await useAsyncData("home", () =>
   queryContent<ParsedParties>("/parties").findOne()
 );
 </script>
 <template>
-  <div class="parties-wrap">
-    <PartyCard v-for="party in data?.parties" :party="party" />
-  </div>
+  <main>
+    <h2>Přehled stran</h2>
+    <div class="parties-wrap">
+      <PartyCard v-for="party in data?.parties" :party="party" />
+    </div>
+  </main>
 </template>
 
 <style scoped>
 .parties-wrap {
-  margin-top: 64px;
   display: flex;
   flex-direction: column;
-  gap: 24px;
+  padding-top: 24px;
+  padding-bottom: 24px;
+  gap: 12px;
 }
 </style>
