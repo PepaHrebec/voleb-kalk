72a3ce0bc43c8745851336ac108575dbedd10b7f
Josef Hrebec <josefhrebec02@gmail.com>
2024-03-15 15:06:21 +0100

    Add page comparing party answers

---
+++ pages/kalkulacka/otazky/porovnani.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import ComparisonCard from "~/components/ComparisonCard.vue";
+import { useQuestionsStore } from "~/scripts/questionsStore";
+
+const questionsStore = useQuestionsStore();
+const { questionsList } = storeToRefs(questionsStore);
+</script>
+<template>
+  <h2>Souhrn</h2>
+  <div class="summary-card-wrap">
+    <ComparisonCard
+      v-for="(question, index) in questionsList"
+      :question-number="index + 1"
+      :question="question"
+      :questions-length="questionsList?.length"
+    />
+  </div>
+</template>
+<style scoped>
+.summary-card-wrap {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  padding-top: 12px;
+  padding-bottom: 24px;
+}
+a {
+  width: 90%;
+}
+</style>
