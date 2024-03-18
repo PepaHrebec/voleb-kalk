78b104dbe3c5e9240ba7a235af9152952e62c0fe
Josef Hrebec <josefhrebec02@gmail.com>
2024-03-18 09:25:04 +0100

    Edit the config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,16 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@pinia/nuxt", "@nuxt/content", "@nuxt/test-utils/module"],
   css: ["@/assets/css/main.css"],
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      htmlAttrs: {
+        lang: "cs",
+      },
+    },
+  },
+  nitro: {
+    compressPublicAssets: true,
+  },
 });
