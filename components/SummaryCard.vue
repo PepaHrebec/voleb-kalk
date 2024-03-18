6c7ee043b5c6158bdc73bda1fded0e8b3b8e38c0
Josef Hrebec <josefhrebec02@gmail.com>
2024-03-18 10:28:10 +0100

    Add star SVGs to summary card

--- components/SummaryCard.vue
+++ components/SummaryCard.vue
@@ -53,6 +53,22 @@ const dropdown = ref(false);
             "
             @send-value="(value) => setAnswer(questionNumber - 1, value)"
           />
+          <svg width="32" height="32" viewBox="0 0 24 24" class="star">
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"
+            />
+          </svg>
+          <svg width="32" height="32" viewBox="0 0 24 24" class="star">
+            <path
+              fill="currentColor"
+              d="m8.243 7.34l-6.38.925l-.113.023a1 1 0 0 0-.44 1.684l4.622 4.499l-1.09 6.355l-.013.11a1 1 0 0 0 1.464.944l5.706-3l5.693 3l.1.046a1 1 0 0 0 1.352-1.1l-1.091-6.355l4.624-4.5l.078-.085a1 1 0 0 0-.633-1.62l-6.38-.926l-2.852-5.78a1 1 0 0 0-1.794 0z"
+            />
+          </svg>
         </div>
       </Transition>
       <div>
@@ -83,8 +99,8 @@ const dropdown = ref(false);
 
 .btns {
   display: flex;
-  flex-direction: row;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   gap: 10px;
   padding: 12px 0;
 }
@@ -120,5 +136,10 @@ h3 {
 .show-enter-from,
 .show-leave-to {
   opacity: 0;
+}
+
+.star {
+  transform: scale(1.2);
+  color: var(--primary-blue);
 }
 </style>
