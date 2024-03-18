ee9ba4026875ca9aff640e4a3b1802dc97473e68
Josef Hrebec <josefhrebec02@gmail.com>
2024-03-18 14:03:09 +0100

    Add important questions calculation

--- composables/useResult.ts
+++ composables/useResult.ts
@@ -1,22 +1,32 @@
 import { useAnswersStore } from "~/scripts/answersStore";
 import type { ParsedAnswers } from "~/types/types";
 
-function innerCalculation(partyAns: number, userAns: number) {
+function innerCalculation(
+  partyAns: number,
+  userAns: number,
+  isImportant: boolean
+) {
   if (partyAns === 5 || userAns === 5 || partyAns === 1 || userAns === 1) {
     if (Math.abs(partyAns - userAns) === 1) {
-      return 0.25;
+      return isImportant ? 0.25 * 2 : 0.25;
     }
   }
 
   if (userAns === 3 && (partyAns === 5 || partyAns === 1)) {
-    return Math.abs(partyAns - userAns);
+    return isImportant
+      ? Math.abs(partyAns - userAns) * 2
+      : Math.abs(partyAns - userAns);
   }
 
   if (userAns === 3) {
-    return (Math.abs(partyAns - userAns) / 3) * 4;
+    return isImportant
+      ? (Math.abs(partyAns - userAns) / 3) * 4 * 2
+      : (Math.abs(partyAns - userAns) / 3) * 4;
   }
 
-  return Math.abs(partyAns - userAns);
+  return isImportant
+    ? Math.abs(partyAns - userAns) * 2
+    : Math.abs(partyAns - userAns);
 }
 
 export default async function useResult() {
@@ -25,7 +35,7 @@ export default async function useResult() {
   );
 
   const answersStore = useAnswersStore();
-  const { answersList } = storeToRefs(answersStore);
+  const { answersList, importantList } = storeToRefs(answersStore);
 
   const resultArr: {
     shortcut: string;
@@ -38,25 +48,41 @@ export default async function useResult() {
     return null;
   }
 
+  // turns "unanswered" into "do not know"
   answersList.value?.forEach((value, index) => {
     if (answersList.value !== undefined) {
       value === 0 ? (answersList.value[index] = 3) : null;
     }
   });
 
+  // counts the amount of "important" answers
+  let importantCount = 0;
+  importantList.value?.forEach((curr) => {
+    if (curr) {
+      importantCount++;
+    }
+  });
+
+  // goes through every party dataset and compares every users answer
+  // with every party answer
   partyAnswersData.data.value.answers.forEach((partyData) => {
     const value = partyData.partyAnswers.reduce((acc, curr, index) => {
       const userValue =
         answersList.value !== undefined ? answersList.value[index] : 3;
-      return acc + innerCalculation(curr, userValue);
+      const isImportant =
+        importantList.value !== undefined ? importantList.value[index] : false;
+      return acc + innerCalculation(curr, userValue, isImportant);
     }, 0);
+    // percentage is calculated as users score divided by the greatest possible score
+    // this is then subtracted from 100 to get the percentage
     resultArr.push({
       shortcut: partyData.shortcut,
       name: partyData.name,
       value: value,
       percentage:
         answersList.value?.length !== undefined
-          ? 100 - value / ((answersList.value?.length * 4) / 100)
+          ? 100 -
+            value / (((answersList.value?.length + importantCount) * 4) / 100)
           : 0,
     });
   });
