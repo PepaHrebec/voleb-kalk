65ace73ab9b57093f023c3b7557d609a10b3ec20
Josef Hrebec <josefhrebec02@gmail.com>
2024-03-23 16:11:32 +0100

    Add star to comparison card

--- components/ComparisonCard.vue
+++ components/ComparisonCard.vue
@@ -26,15 +26,33 @@ const templateText = [
 ];
 
 const answersStore = useAnswersStore();
-const { answersList } = storeToRefs(answersStore);
+const { answersList, importantList } = storeToRefs(answersStore);
 
 const dropdown = ref(false);
 </script>
 <template>
   <div class="summary-wrap">
     <div class="top">
-      <div class="question-number">
-        {{ questionNumber }} / {{ questionsLength }}
+      <div class="upper-row">
+        <div class="question-number">
+          {{ questionNumber }} / {{ questionsLength }}
+        </div>
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+          class="star star-top"
+          v-if="
+            importantList !== undefined
+              ? importantList[questionNumber - 1]
+              : false
+          "
+        >
+          <path
+            fill="currentColor"
+            d="m8.243 7.34l-6.38.925l-.113.023a1 1 0 0 0-.44 1.684l4.622 4.499l-1.09 6.355l-.013.11a1 1 0 0 0 1.464.944l5.706-3l5.693 3l.1.046a1 1 0 0 0 1.352-1.1l-1.091-6.355l4.624-4.5l.078-.085a1 1 0 0 0-.633-1.62l-6.38-.926l-2.852-5.78a1 1 0 0 0-1.794 0z"
+          />
+        </svg>
       </div>
       <h3>
         <NuxtLink :to="`/kalkulacka/otazky/${questionNumber}`" class="title">{{
@@ -81,6 +99,11 @@ const dropdown = ref(false);
 <style scoped>
 .top {
   padding: 12px;
+}
+
+.upper-row {
+  display: flex;
+  flex-direction: row;
 }
 
 .dropdown-wrap {
@@ -146,5 +169,16 @@ h3 {
 .show-enter-from,
 .show-leave-to {
   opacity: 0;
+}
+
+.star {
+  transform: scale(1.2);
+  color: var(--primary-blue);
+  cursor: pointer;
+}
+
+.star-top {
+  height: 1em;
+  transform: translateY(2px);
 }
 </style>
